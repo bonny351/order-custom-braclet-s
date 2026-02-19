5bed72fa6871fd337ff24acf84a415660b2a9fbe
bonny351 <carr.m.isaiah@outlook.com>
2026-02-19 08:48:33 -0800

    Add Firebase configuration and Firestore initialization
    Initialize Firebase app with configuration settings.

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+firebase.initializeApp({
+  apiKey: "AIzaSyCiZuaANb1lrgdn-aARGJ_TRhgzPPUug58",
+  authDomain: "bracelets-and-color.firebaseapp.com",
+  projectId: "bracelets-and-color",
+  storageBucket: "bracelets-and-color.appspot.com",
+  messagingSenderId: "382292570673",
+  appId: "1:382292570673:web:1c966e2e7e8fc2efa31b10"
+});
+
+const db = firebase.firestore();
